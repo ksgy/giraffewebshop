838629749c9278bb5413e74ab876ac404a38b802
ksgy <ksgy@x-plane.hu>
2015-12-07 00:28:29 +0000

    Changing prices

--- app/scripts/services/giraffes.js
+++ app/scripts/services/giraffes.js
@@ -15,13 +15,13 @@ angular.module('giraffeWebshopApp')
         id: 1,
         name: 'West African Giraffe',
         description: 'They are also called the Nigerian Giraffe due to the fact that is where most of them are found.',
-        price: 1337
+        price: 400
       },
       {
         id: 2,
         name: 'Somali Giraffe',
         description: 'The Giraffa camelopardalis reticulata is the one most often found in zoos around the world',
-        price: 1337
+        price: 200
       },
       {
         id: 3,
@@ -31,15 +31,15 @@ angular.module('giraffeWebshopApp')
       },
       {
         id: 4,
-        name: 'West African giraffe',
-        description: 'The Masai Giraffe is extremely powerful due to the design of the legs and the hooves.',
-        price: 1337
+        name: 'East African giraffe',
+        description: 'They are living in East Africa',
+        price: 404
       },
       {
         id: 5,
         name: 'Rothschild Giraffe',
         description: 'They can also weight about 2,800 pounds with the males weighing more than the females by several hundred pounds.',
-        price: 1337
+        price: 301
       }
     ];
 
